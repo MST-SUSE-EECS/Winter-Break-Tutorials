eaf4b0b447a2e362d0c6a1128d5bc9f6c864eb2d
Adam Camerer <ajc3xc@umsystem.edu>
2024-01-14 06:21:27 -0600

    starting work on code

---
+++ database.py
@@ -0,0 +1,7 @@
+import sqlalchemy as _sql
+import sqlalchemy.ext.delcarative as declarative
+import sqlalchemy.orm as _orm
+
+DATABASE_URL = "postgresql://myuser:password@localhost/fastapi_database"
+
+
